02afbf383f2e6cef8e0a54734bf22038383584ea
Hamzaoui Mohamed Nour <46738243+hamzaouimednour@users.noreply.github.com>
2023-12-19 10:10:49 +0100

    mysql

--- mysql/build.sh
+++ mysql/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker image rm dev-mysql:1.0 -f  && \
+docker image rm dev-mysql:1.0 -f && \
 docker build . -t dev-mysql:1.0 && \
 docker run --detach --name=dev-mysql --publish 3306:3306 dev-mysql:1.0 && \
 docker ps
